12f3776e3c1509bc56d1c90413818fd3c93a43bf
aryormdhn <aryoromadhon8@gmail.com>
2024-06-20 09:51:24 +0700

    add image on home.py

--- home.py
+++ home.py
@@ -1,5 +1,22 @@
 import streamlit as st
 
 def show_home():
+    st.markdown("""
+        <style>
+        .center {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+    
+    st.image("logo.png", width=200,
+             
+             )  # Mengatur lebar gambar menjadi 200 piksel
     st.markdown("<h1 style='text-align: center; color: white;'>Aplikasi Deteksi Malaria</h1>", unsafe_allow_html=True)
     st.markdown("<p style='text-align: center; color: grey;'>Selamat datang di Aplikasi Deteksi Malaria. Gunakan menu untuk menavigasi aplikasi.</p>", unsafe_allow_html=True)
+
+# Contoh cara menjalankan halaman home
+if __name__ == "__main__":
+    show_home()
